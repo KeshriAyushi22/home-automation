a281e0cfed1b8153dc0087f8fe27acc11e33ea13
KeshriAyushi22 <keshri.ayushi10@gmail.com>
2019-05-14 16:37:56 +0530

    mail-authentication failed exception fixed 2

--- src/main/java/ayu/arduino/service/Activation.java
+++ src/main/java/ayu/arduino/service/Activation.java
@@ -13,69 +13,61 @@ import ayu.arduino.to.LoginDetails;
 
 public class Activation extends Authenticator {
 
-public static void doAccountActivation(LoginDetails request){
-	 
-		      String from = "subhamnaik15@gmail.com";   //mail id which dont have 2 way verification with phone.
-		      String to = request.getEmail();
-		      String password = "lucky1234567";
-		      String token= request.getPasswordToken();
+	public static void doAccountActivation(LoginDetails request){
 
-		      String host = "127.0.0.1";//or IP address  
-		  
-		    //Get properties object    
-	          Properties props = new Properties();    
-	          props.put("mail.smtp.host", "smtp.gmail.com");    
-	          props.put("mail.smtp.socketFactory.port", "465");    
-	          props.put("mail.smtp.socketFactory.class",    
-	                    "javax.net.ssl.SSLSocketFactory");    
-	          props.put("mail.smtp.auth", "true");    
-	          props.put("mail.smtp.port", "465");    
-	          //get Session   
-	          Session session = Session.getDefaultInstance(props,    
-	           new javax.mail.Authenticator() {    
-	           protected PasswordAuthentication getPasswordAuthentication() {    
-	           return new PasswordAuthentication(from,password);  
-	           }    
-	          }); 
-	          
-	          //compose message    
-	          try {    
-	           MimeMessage message = new MimeMessage(session);  
-	           message.setFrom(new InternetAddress(from));
-	           message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));    
-	           message.setSubject("new subject");    
-	           /*message.setText("your activation link is: "+ "http://localhost:48080/arduino/web/iot/changePassword?key1="+to+"&key2="+token);*/  
-	           message.setText("your activation link is: "+ "https://home-automation-iot.herokuapp.com/web/iot/changePassword?key1="+to+"&key2="+token);
-	           
-	           //send message  
-	           Transport.send(message);    
-	           System.out.println("message sent successfully");    
-	          } catch (MessagingException e) {
-	        	  throw new RuntimeException(e);}    
-	             
-		     
-		   } 
+		String from = "subhamnaik15@gmail.com";   //mail id which dont have 2 way verification with phone.
+		String to = request.getEmail();
+		String password = "lucky1234567";
+		String token= request.getPasswordToken();
 
-@Override
-public PasswordAuthentication getPasswordAuthentication() {
- String username = "subhamnaik15@gmail.com";
- String password = "lucky1234567";
-    if ((username != null) && (username.length() > 0) && (password != null) 
-      && (password.length   () > 0)) {
+		String host = "127.0.0.1";//or IP address  
 
-        return new PasswordAuthentication(username, password);
-    }
+		//Get properties object    
+		Properties props = new Properties();    
+		props.put("mail.smtp.host", "smtp.gmail.com");    
+		props.put("mail.smtp.socketFactory.port", "465");    
+		props.put("mail.smtp.socketFactory.class",    
+				"javax.net.ssl.SSLSocketFactory");    
+		props.put("mail.smtp.auth", "true");    
+		props.put("mail.smtp.port", "465");    
+		//get Session   
 
-    return null;
+		if ((from != null) && (from.length() > 0) && (password != null) 
+				&& (password.length   () > 0)) {
+			Session session = Session.getDefaultInstance(props,    
+					new javax.mail.Authenticator() {    
+				protected PasswordAuthentication getPasswordAuthentication() {    
+					return new PasswordAuthentication(from,password);  
+
+				}    
+			}); 
+
+			//compose message    
+			try {    
+				MimeMessage message = new MimeMessage(session);  
+				message.setFrom(new InternetAddress(from));
+				message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));    
+				message.setSubject("new subject");    
+				/*message.setText("your activation link is: "+ "http://localhost:48080/arduino/web/iot/changePassword?key1="+to+"&key2="+token);*/  
+				message.setText("your activation link is: "+ "https://home-automation-iot.herokuapp.com/web/iot/changePassword?key1="+to+"&key2="+token);
+
+				//send message  
+				Transport.send(message);    
+				System.out.println("message sent successfully");    
+			} catch (MessagingException e) {
+				throw new RuntimeException(e);}    
+
+
+		} 
+
+
+
+	}
 }
 
-		
-		
-		}
-		
 
 
-	
-	
-	
+
+
+
 
