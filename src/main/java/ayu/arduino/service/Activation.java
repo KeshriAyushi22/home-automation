b332a770ba0ec426831bc309f0ac654c0d917134
KeshriAyushi22 <keshri.ayushi10@gmail.com>
2019-05-15 17:07:51 +0530

    javax Mail Authntication error fixing stage

--- src/main/java/ayu/arduino/service/Activation.java
+++ src/main/java/ayu/arduino/service/Activation.java
@@ -25,11 +25,11 @@ public class Activation extends Authenticator {
 		//Get properties object    
 		Properties props = new Properties();    
 		props.put("mail.smtp.host", "smtp.gmail.com");    
-		props.put("mail.smtp.socketFactory.port", "465");    
+		props.put("mail.smtp.socketFactory.port", "587");    
 		props.put("mail.smtp.socketFactory.class",    
 				"javax.net.ssl.SSLSocketFactory");    
 		props.put("mail.smtp.auth", "true");    
-		props.put("mail.smtp.port", "465");    
+		props.put("mail.smtp.port", "587");    
 		//get Session   
 
 		if ((from != null) && (from.length() > 0) && (password != null) 
