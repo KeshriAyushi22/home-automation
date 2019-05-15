90f0209e174b41aa64f6579d91f99dfbe7e84223
KeshriAyushi22 <keshri.ayushi10@gmail.com>
2019-05-15 17:21:26 +0530

    mail authentication fixing2

--- src/main/java/ayu/arduino/service/Activation.java
+++ src/main/java/ayu/arduino/service/Activation.java
@@ -24,12 +24,24 @@ public class Activation extends Authenticator {
 
 		//Get properties object    
 		Properties props = new Properties();    
-		props.put("mail.smtp.host", "smtp.gmail.com");    
+	/*	props.put("mail.smtp.host", "smtp.gmail.com");
+		props.setProperty("mail.transport.protocol", "smtp"); 
 		props.put("mail.smtp.socketFactory.port", "587");    
-		props.put("mail.smtp.socketFactory.class",    
-				"javax.net.ssl.SSLSocketFactory");    
+		props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");    
 		props.put("mail.smtp.auth", "true");    
-		props.put("mail.smtp.port", "587");    
+		props.put("mail.smtp.port", "587");  
+	    props.put("mail.smtp.socketFactory.fallback", "false");
+	    props.put("mail.smtp.starttls.enable", "true");*/
+	    
+	    props.setProperty("mail.transport.protocol", "smtp");     
+	    props.setProperty("mail.host", "smtp.gmail.com");  
+	    props.put("mail.smtp.auth", "true");  
+	    props.put("mail.smtp.port", "465");  
+	    props.put("mail.debug", "true");  
+	    props.put("mail.smtp.socketFactory.port", "465");  
+	    props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");  
+	    props.put("mail.smtp.socketFactory.fallback", "false"); 
+	    
 		//get Session   
 
 		if ((from != null) && (from.length() > 0) && (password != null) 
